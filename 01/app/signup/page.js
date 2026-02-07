e46046a0662a98e110d6c995c83879cb601f89ed
krishnaidu2556@gmail.com <krishnaidu2556@gmail.com>
2026-02-07 11:22:36 +0530

    01

---
+++ 01/app/signup/page.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p>SignUp page</p>
+    </div>
+  );
+}   
